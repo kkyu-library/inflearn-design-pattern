1be777d948476520777b8d8b11804c6488fd17e6
kkyu8925 <kkyu8925@gmail.com>
2022-08-17 23:47:35 +0900

    Singleton

---
+++ example/src/main/kotlin/com/example/_01_creational_patterns/_01_singleton/Settings.kt
@@ -0,0 +1,3 @@
+package com.example._01_creational_patterns._01_singleton
+
+object Settings {}
---
+++ example/src/test/kotlin/com/example/_01_creational_patterns/_01_singleton/SettingsTest.kt
@@ -0,0 +1,13 @@
+package com.example._01_creational_patterns._01_singleton
+
+import org.junit.jupiter.api.Test
+
+internal class SettingsTest {
+
+    @Test
+    fun main() {
+        val settings1 = Settings
+        val settings2 = Settings
+        assert(settings1 == settings2)
+    }
+}
