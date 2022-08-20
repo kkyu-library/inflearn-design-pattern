e65f8199fda824a3ad3183776dfb75efbd5a58bb
kkyu8925 <kkyu8925@gmail.com>
2022-08-20 22:00:17 +0900

    Factory method

--- example/src/main/kotlin/com/example/_01_creational_patterns/_02_factory_method/kt/_02_after/Ship.kt
+++ example/src/main/kotlin/com/example/_01_creational_patterns/_02_factory_method/kt/_02_after/Ship.kt
@@ -1,7 +1,7 @@
 package com.example._01_creational_patterns._02_factory_method.kt._02_after
 
-import com.example._01_creational_patterns._03_abstract_factory._02_after.Anchor
-import com.example._01_creational_patterns._03_abstract_factory._02_after.Wheel
+import com.example._01_creational_patterns._03_abstract_factory.kt._02_after.Anchor
+import com.example._01_creational_patterns._03_abstract_factory.kt._02_after.Wheel
 
 abstract class Ship(
     open val name: String,
