231e3767dd2aebfc8fb9e16d6ab42e3e96675b84
kkyu8925 <kkyu8925@gmail.com>
2022-08-22 22:36:52 +0900

    builder

---
+++ example/src/main/kotlin/com/example/_01_creational_patterns/_04_builder/java/_03_java/LombokExample.java
@@ -0,0 +1,22 @@
+package com.example._01_creational_patterns._04_builder.java._03_java;
+
+//import lombok.Builder;
+
+// lombok 의존성 필요
+//@Builder
+public class LombokExample {
+
+    private String title;
+
+    private int nights;
+
+    private int days;
+
+    public static void main(String[] args) {
+//        LombokExample trip = LombokExample.builder()
+//                .title("여행")
+//                .nights(2)
+//                .days(3)
+//                .build();
+    }
+}
