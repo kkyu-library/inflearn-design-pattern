7bf90d30c77908de6eac6f4668587c47a30f09c4
kkyu8925 <kkyu8925@gmail.com>
2022-08-29 23:02:14 +0900

    Singleton

--- example/src/main/kotlin/com/example/_01_creational_patterns/_01_singleton/kt/Settings.kt
+++ example/src/main/kotlin/com/example/_01_creational_patterns/_01_singleton/kt/Settings.kt
@@ -1,3 +1,9 @@
 package com.example._01_creational_patterns._01_singleton.kt
 
 object Settings {}
+
+fun main() {
+    val settings1 = Settings
+    val settings2 = Settings
+    println(settings1 == settings2)
+}
