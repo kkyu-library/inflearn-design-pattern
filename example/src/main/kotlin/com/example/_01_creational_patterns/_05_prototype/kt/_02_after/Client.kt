d0debc3db66e4e2dd02f7a69e1c346a5ec99920f
kkyu8925 <kkyu8925@gmail.com>
2022-08-29 23:09:24 +0900

    prototype

---
+++ example/src/main/kotlin/com/example/_01_creational_patterns/_05_prototype/kt/_02_after/Client.kt
@@ -0,0 +1,26 @@
+package com.example._01_creational_patterns._05_prototype.kt._02_after
+
+fun main() {
+    val repository = GithubRepository("whiteship", "live-study")
+
+    val githubIssue = GithubIssue(
+        repository = repository,
+        id = 1,
+        title = "1주차 과제: JVM은 무엇이며 자바 코드는 어떻게 실행하는 것인가."
+    )
+
+    val url = githubIssue.url
+    println(url)
+
+    val clone = githubIssue.copy()
+    println(clone.url)
+
+    repository.user = "Keesun"
+
+    println("clone !== githubIssue : ${clone !== githubIssue}")
+    println("clone == githubIssue : ${clone == githubIssue}")
+    println("clone.repository === githubIssue.repository : ${clone.repository === githubIssue.repository}")
+
+    println(githubIssue.url)
+    println(clone.url)
+}
