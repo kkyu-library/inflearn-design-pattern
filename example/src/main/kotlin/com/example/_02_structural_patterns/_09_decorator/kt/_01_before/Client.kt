abfbf38172952fa740ad1ae1f6e8e46beaf2511a
kkyu8925 <kkyu8925@gmail.com>
2022-08-30 23:33:23 +0900

    decorator

--- example/src/main/kotlin/com/example/_02_structural_patterns/_09_decorator/kt/_01_before/Client.kt
+++ example/src/main/kotlin/com/example/_02_structural_patterns/_09_decorator/kt/_01_before/Client.kt
@@ -7,3 +7,15 @@ class Client(
         commentService.addComment(comment)
     }
 }
+
+fun main() {
+    val client = Client(SpamFilteringCommentService())
+    client.writeComment("오징어게임")
+    client.writeComment("보는게 하는거 보다 재밌을 수가 없지...")
+    client.writeComment("http://whiteship.me")
+
+    val client1 = Client(TrimmingCommentService())
+    client1.writeComment("오징어게임")
+    client1.writeComment("보는게 하는거 보다 재밌을 수가 없지...")
+    client1.writeComment("http://whiteship.me")
+}
--- example/src/main/kotlin/com/example/_02_structural_patterns/_09_decorator/kt/_02_after/Client.kt
+++ example/src/main/kotlin/com/example/_02_structural_patterns/_09_decorator/kt/_02_after/Client.kt
@@ -8,3 +8,24 @@ class Client(
         commentService.addComment(comment)
     }
 }
+
+private const val enabledSpamFilter = true
+
+private const val enabledTrimming = true
+
+fun main() {
+    var commentService: CommentService = DefaultCommentService()
+
+    if (enabledSpamFilter) {
+        commentService = SpamFilteringCommentDecorator(commentService)
+    }
+
+    if (enabledTrimming) {
+        commentService = TrimmingCommentDecorator(commentService)
+    }
+
+    val client = Client(commentService)
+    client.writeComment("오징어게임")
+    client.writeComment("보는게 하는거 보다 재밌을 수가 없지...")
+    client.writeComment("http://whiteship.me")
+}
